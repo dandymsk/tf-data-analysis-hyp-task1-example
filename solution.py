fd0215323000260059840071973a4c691a3d6906
dandymsk <96170484+dandymsk@users.noreply.github.com>
2023-04-11 19:59:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-import math as mth
-import scipy.stats as st
+from scipy.stats import ttest_ind
 
 chat_id = 841977 # Ваш chat ID, не меняйте название переменной
 
@@ -9,14 +8,10 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    p1=x_success/x_cnt
-    p2=y_success/y_cnt
+    sample1=x_success/x_cnt
+    sample2=y_success/y_cnt
     alpha = 0.06
-    p_combined = (p1*x_cnt+p2*y_cnt)/(x_cnt+y_cnt)
-    z_value = (p1-p2)/(mth.sqrt(p_combined*(1-p_combined)*(1/x_cnt+1/y_cnt)))
-    distr = st.norm(0, 1)
-    p_value = (1 - distr.cdf(abs(z_value))) * 2
-    
+    t_stat, p_value = ttest_ind(sample1, sample2,equal_var=False)
     if p_value <= alpha:
         return True
     else:
